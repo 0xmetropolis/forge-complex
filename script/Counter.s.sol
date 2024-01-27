95cd537d04636492501b29891f4f317e2f29c06c
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2024-01-26 17:09:21 -0700

    feat: make complex contracts and deploy script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,34 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Counter, CrazyInitializer, ChangeType, CounterArgs} from "src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        Counter counter = new Counter(CounterArgs({number: 0, boolean: false}));
+        counter.setNumber(CounterArgs({number: 1, boolean: false}));
+
+        address[] memory wut = new address[](2);
+
+        counter.increment(wut);
+
+        CounterArgs[] memory yikes = new CounterArgs[](2);
+
+        yikes[0] = CounterArgs({number: 1, boolean: false});
+        yikes[1] = CounterArgs({number: 2, boolean: false});
+
+        counter.hereWeGo(yikes);
+
+        counter.change(ChangeType.Decrement, 1);
+
+        new CrazyInitializer(
+            ChangeType.Increment,
+            yikes
+        );
+
+        vm.stopBroadcast();
     }
 }
--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+struct CounterArgs {
+    uint256 number;
+    bool boolean;
+}
+
+enum ChangeType {
+    Increment,
+    Decrement
+}
+
 contract Counter {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    constructor(CounterArgs memory args) {
+        number = args.number;
     }
 
-    function increment() public {
-        number++;
+    function setNumber(CounterArgs memory args) public {
+        number = args.number;
+    }
+
+    function simple(uint256 simpleIncrement) public {
+        number += simpleIncrement;
+    }
+
+    function change(ChangeType changeType, uint256 changeAmount) public {
+        if (changeType == ChangeType.Increment) {
+            number += changeAmount;
+        } else {
+            number -= changeAmount;
+        }
+    }
+
+    function increment(address[] calldata timesAmount) public {
+        number += timesAmount.length;
+    }
+
+    function hereWeGo(CounterArgs[] calldata yikes) public {
+        for (uint256 i = 0; i < yikes.length; i++) {
+            number += yikes[i].number;
+        }
+    }
+}
+
+contract CrazyInitializer {
+    constructor(ChangeType changeType, CounterArgs[] memory args) {
+        changeType;
+        args;
     }
 }
